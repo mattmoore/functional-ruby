acf0ada894130e0dccb9aa336a53bd5498a5ad8a
Matt Moore <mattmoore@carbonhelix.com>
2018-11-29 09:51:45 -0600

    Functional map

---
+++ map/functional_map.rb
@@ -0,0 +1,21 @@
+require 'benchmark'
+require 'active_support/inflector'
+
+class Array
+  def mymap(&f)
+    self.reduce([]) { |xs, x| xs << f.(x); xs }
+  end
+end
+
+collection = (1..3).to_a
+f = -> (x) { x * 2 }
+
+examples = {
+  mymap: collection.mymap{ |x| f.(x) }
+}
+
+examples.keys.each do |example|
+  result = nil
+  benchmark = Benchmark.measure { result = examples[example] }
+  puts "#{example.to_s.titleize}:".ljust(20) + "#{result} #{benchmark}"
+end
